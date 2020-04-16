1efe02cda27033cd86be92ab543ec9439890508f
Aidan B <aidanrichbennett+github@gmail.com>
2020-04-15 22:16:48 -0400

    Simple platforms, gravity, and controls. Also fixed the scrollbar on max height in Chrome and fits to window -- We probably want to set a fixed viewport size at some point Turned default JavaScript check off -- Too many 'errors' from libraries, manually toggled now
    Co-authored-by: gaberaiber <gabedefender8@gmail.com>
    Co-authored-by: Dobry144 <i77vaz@gmail.com>
    Co-authored-by: obycode <brice@obycode.com>

--- main.js
+++ main.js
@@ -1,31 +1,79 @@
+// @ts-check
 /// <reference path="./p5.d.ts" />
 /// <reference path="./p5.global-mode.d.ts" />
 
+//import { collide } from "./libraries/p5.play";
+
+// Groups
+var grounds;
+var players;
+
+
+
 function setup() {
-    // getAudioContext().suspend();
-    createCanvas(innerWidth, innerHeight - 5);
+    grounds = new Group();
+    players = new Group();
+
+    createCanvas(innerWidth, innerHeight);
+
+    // Create platforms
+    var ground = createSprite(width / 2, height - 30, width - 200, 30);
+    ground.addToGroup(grounds);
+    var ground2 = createSprite(width - 200, height - 150, 200, 30);
+    ground2.addToGroup(grounds);
+    //var wall = createSprite(width)
 }
+
+
 
 function draw() {
-    background(54, 173, 136);
-    
-    drawSprites();
-    
-    fill("white");
-    var greeting = "Hello world!";
-    hello(greeting);
+    background(0, 30, 30);
 
-    /**
-     * shows text
-     * @param {string} textToShow the message
-     */
-    function hello(textToShow) {
-        textAlign(CENTER);
-        textSize(36);
-        text(textToShow, width / 2, height / 2);
+    for (player of players) {
+        // Ground detection
+        if (player.collide(grounds)) {
+            // Stop on ground
+            player.velocity.y = 0;
+            
+            if (keyDown(UP_ARROW)) {
+                // Jump
+                player.velocity.y = -10;
+            }
+        }
+        else {
+            // Gravity
+            player.velocity.y += 0.4;
+        }
+
+
+        // Move players with arrow keys
+        if (keyDown(LEFT_ARROW)) player.velocity.x -= 1;
+        else if (keyDown(RIGHT_ARROW)) player.velocity.x += 1;
+        // "Friction"
+        else {
+            if (player.velocity.x > 1.5) player.velocity.x -= 3;
+            else if (player.velocity.x < -1.5) player.velocity.x += 3;
+            else player.velocity.x = 0;
+        }
+
+        // Remove dead players
+        if (player.position.y > 1000) {
+            player.remove();
+        }
     }
+
+    drawSprites();
+
+    // Show some diagnostic data
+    fill("white");
+    text(round(frameRate()), 10, 20);
+    text(players.length, 10, 40);
 }
 
+
+
 function mousePressed() {
-    var sprite = createSprite(mouseX, mouseY, 30, 30);
+    // Create players on click
+    player = createSprite(mouseX, mouseY, 30, 30);
+    player.addToGroup(players);
 }
