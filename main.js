61708f185621510e649b29066f83e3b05ea8e967
Aidan B <aidanrichbennett+github@gmail.com>
2020-04-19 17:57:55 -0400

    Redo floors - Add function to create sprites for each side and floor   -- Necessary to kill momentum (I think) selectively and for jumping - Add debugging

--- main.js
+++ main.js
@@ -2,78 +2,148 @@
 /// <reference path="./p5.d.ts" />
 /// <reference path="./p5.global-mode.d.ts" />
 
-//import { collide } from "./libraries/p5.play";
-
 // Groups
-var grounds;
-var players;
+let blocks
+let tops
+let bottoms
+let lefts
+let rights
+//let players
+let player
+let canvas
+let showMXY
+let MAX_SPEED = 15
+let GRAVITY = 0.4
+let JUMP = 10
 
 
 
 function setup() {
-    grounds = new Group();
-    players = new Group();
+    blocks = new Group()
+    tops = new Group()
+    bottoms = new Group()
+    lefts = new Group()
+    rights = new Group()
+    //players = new Group()
+    showMXY = false
 
-    createCanvas(innerWidth, innerHeight);
+    canvas = createCanvas(innerWidth, innerHeight)
+    canvas.mouseOver(MXYon)
+    canvas.mouseOut(MXYoff)
 
+    newBlock(-2, 0, 0, 800)
     // Create platforms
-    var ground = createSprite(width / 2, height - 30, width - 200, 30);
-    ground.addToGroup(grounds);
-    var ground2 = createSprite(width - 200, height - 150, 200, 30);
-    ground2.addToGroup(grounds);
-    //var wall = createSprite(width)
+    newBlock(0, 600, 800, 30)
+    newBlock(500, 500, 300, 30)
+    newBlock(770, 300, 30, 300)
+    newBlock(700, 400, 30, 30)
+    
+    player = createSprite(100, 400, 30, 30)
 }
 
 
 
 function draw() {
-    background(0, 30, 30);
+    background(0, 30, 30)
 
-    for (player of players) {
-        // Ground detection
-        if (player.collide(grounds)) {
+    if (player.collide(tops) && player.velocity) {
+        if (keyDown(UP_ARROW)) {
+            // Jump
+            player.velocity.y = -JUMP
+            player.position.y = player.position.y - 1
+        }
+        else {
             // Stop on ground
-            player.velocity.y = 0;
-            
-            if (keyDown(UP_ARROW)) {
-                // Jump
-                player.velocity.y = -10;
-            }
+            player.velocity.y = 0
         }
-        else {
-            // Gravity
-            player.velocity.y += 0.4;
-        }
-
-
-        // Move players with arrow keys
-        if (keyDown(LEFT_ARROW)) player.velocity.x -= 1;
-        else if (keyDown(RIGHT_ARROW)) player.velocity.x += 1;
-        // "Friction"
-        else {
-            if (player.velocity.x > 1.5) player.velocity.x -= 3;
-            else if (player.velocity.x < -1.5) player.velocity.x += 3;
-            else player.velocity.x = 0;
-        }
-
-        // Remove dead players
-        if (player.position.y > height) {
-            player.remove();
-        }
+        console.log("top")
+    }
+    else {
+        // Gravity
+        player.velocity.y += GRAVITY
     }
 
-    drawSprites();
+    if (player.collide(bottoms) && player.velocity.y < 0) {
+        player.velocity.y = 0
+        console.log("bottom")
+    }
+    if (player.collide(lefts) && player.velocity.x > 0) {
+        player.velocity.x = 0
+        console.log("left")
+    }
+    if (player.collide(rights) && player.velocity.x < 0) {
+        player.velocity.x = 0
+        console.log("right")
+    }
 
-    // Show some diagnostic data
-    fill("white");
-    text(round(frameRate()), 10, 20);
-    text(players.length, 10, 40);
+
+    // Move players with arrow keys
+    if (keyDown(LEFT_ARROW) && player.velocity.x <= 0 && player.velocity.x > -MAX_SPEED) player.velocity.x -= 1
+    else if (keyDown(RIGHT_ARROW) && player.velocity.x >= 0 && player.velocity.x < MAX_SPEED) player.velocity.x += 1
+    // "Friction"
+    else {
+        if (player.velocity.x > 2) player.velocity.x -= 3
+        else if (player.velocity.x < -2) player.velocity.x += 3
+        else player.velocity.x = 0
+    }
+
+    // Reset dead player
+    if (player.position.y > height) {
+        //player.remove()
+        player.position.x = 100
+        player.position.y = 400
+        player.velocity.x = 0
+        player.velocity.y = 0
+    }
+
+    drawSprites()
+
+    var debug = []
+    debug.push(round(getFrameRate()))
+    debug.push(str(round(player.position.x)) + ", " + str(round(player.position.y)))
+    debug.push(str(round(player.velocity.x)) + ", " + str(round(player.velocity.y)))
+
+    displayDebug(debug)
+
+    if (showMXY) text(str(mouseX) + ", " + str(mouseY), mouseX, mouseY)
 }
 
 
 
-function mousePressed() {
-    // Create players on click
-    player = createSprite(mouseX, mouseY, 30, 30);
-    player.addToGroup(players);
+function MXYon() {
+    showMXY = true
+}
+
+function MXYoff() {
+    showMXY = false
+}
+
+/**
+ * @param {number} x
+ * @param {number} w
+ * @param {number} y
+ * @param {number} h
+ */
+function newBlock(x, y, w, h) {
+    let block = createSprite(x + (w / 2), y + (h / 2), w, h)
+    block.addToGroup(blocks)
+    let top = createSprite(x + (w / 2), y, w, 1)
+    top.addToGroup(tops)
+    let bottom = createSprite(x + (w / 2), y + h, w, 1)
+    bottom.addToGroup(bottoms)
+    let left = createSprite(x - 1, y + (h / 2), 1, h)
+    left.addToGroup(lefts)
+    let right = createSprite(x + w + 1, y + (h / 2), 1, h)
+    right.addToGroup(rights)
+}
+
+
+
+function displayDebug(array) {
+    camera.off()
+    fill("white")
+    textSize(12)
+    for (var i = 0; i < array.length; i++) {
+        text(str(array[i]), 8, 20 * (i + 1))
+    }
 }
